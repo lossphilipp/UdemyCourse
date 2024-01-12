fe0ffa38c08b3d11dfda5d815c4c9b2d74064297
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-01-12 20:31:30 +0100

    Added Prototype template

---
+++ 04_Prototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            // todo
+        }
+    }
+}

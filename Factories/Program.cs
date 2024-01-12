df247c62dcd902b32d81b9bc31079216c2f1a7a1
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-01-12 19:28:18 +0100

    implemented factory solution

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Coding.Exercise
 {
+    public class PersonFactory
+    {
+        public List<Person> createdPersons = new List<Person>();
+
+        public Person CreatePerson(string name)
+        {
+            var p = new Person(createdPersons.Count, name);
+            createdPersons.Add(p);
+            return p;
+        }
+    }
+
     public class Person
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        internal Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }

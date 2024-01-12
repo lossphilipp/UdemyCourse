71784ab2c2d7cbb480e690d9f0ef9bb7a9598094
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-01-12 21:00:12 +0100

    implemented prototype

--- 04_Prototype/Program.cs
+++ 04_Prototype/Program.cs
@@ -5,15 +5,45 @@ namespace Coding.Exercise
     public class Point
     {
         public int X, Y;
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point DeepCopy()
+        {
+            return new Point(X, Y);
+        }
     }
 
     public class Line
     {
         public Point Start, End;
 
+        // Needed because the tests call this
+        public Line()
+        {
+            Start = new Point();
+            End = new Point();
+        }
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
         public Line DeepCopy()
         {
-            // todo
+            return new Line(Start.DeepCopy(), End.DeepCopy());
         }
     }
 }

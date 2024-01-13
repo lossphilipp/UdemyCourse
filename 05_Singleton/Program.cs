de37c15fb7225e1970b59653984c727eb6b9b616
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-01-13 17:18:01 +0100

    added singleton template

---
+++ 05_Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            // todo
+        }
+    }
+}

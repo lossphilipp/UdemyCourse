e589f83f3b5efd4b139f5ff8345ac8cbc6f6b353
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-01-13 17:22:58 +0100

    implemented singleton

--- 05_Singleton/Program.cs
+++ 05_Singleton/Program.cs
@@ -4,9 +4,17 @@ namespace Coding.Exercise
 {
     public class SingletonTester
     {
+        private static int instanceId = 0;
+
         public static bool IsSingleton(Func<object> func)
         {
-            // todo
+            if (instanceId != func.GetHashCode())
+            {
+                instanceId = func.GetHashCode();
+                return true;
+            }
+
+            return false;
         }
     }
 }

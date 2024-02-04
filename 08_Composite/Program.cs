404eb36ef337d17ddda028f357873a471000d8b3
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-04 11:33:13 +0100

    added composite template

---
+++ 08_Composite/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public interface IValueContainer
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
+        }
+    }
+}

fb5b6b08cea2d3bbd5103cd3508468044de45a24
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-05 15:34:35 +0100

    added decorator template

---
+++ 09_Decorator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        public int Age
+        {
+            // todo :)
+        }
+
+        public string Fly()
+        {
+            // todo
+        }
+
+        public string Crawl()
+        {
+            // todo
+        }
+    }
+}

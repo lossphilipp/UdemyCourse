2f470e5936861ca84da01797304fe0127f5658ab
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-07 19:23:27 +0100

    added flyweight template

---
+++ 11_Flyweight/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Sentence
+    {
+        public Sentence(string plainText)
+        {
+            // todo
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                // todo
+            }
+        }
+
+        public override string ToString()
+        {
+            // output formatted text here
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+}

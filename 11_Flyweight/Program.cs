8b066e00fe042f3e04e74524bffa6702bc364ad8
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-07 19:51:42 +0100

    implemented flyweight

--- 11_Flyweight/Program.cs
+++ 11_Flyweight/Program.cs
@@ -1,25 +1,54 @@
-﻿using System;
+﻿using Coding.Exercise;
+using System;
+using System.Text;
+
+// Sentence sentence = new Sentence("hello world");
+// sentence[1].Capitalize = true;
+// Console.WriteLine(sentence); // writes "hello WORLD"
 
 namespace Coding.Exercise
 {
     public class Sentence
     {
+        private string[] _words;
+        private WordToken[] _wordTokens;
+
         public Sentence(string plainText)
         {
-            // todo
+            _words = plainText.Split(' ');
+            _wordTokens = new WordToken[_words.Length];
+
+            for (int i = 0; i < _wordTokens.Length; i++)
+            {
+                _wordTokens[i] = new WordToken() { Capitalize = false };
+            }
         }
 
         public WordToken this[int index]
         {
             get
             {
-                // todo
+                return _wordTokens[index];
             }
         }
 
         public override string ToString()
         {
-            // output formatted text here
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < _words.Length; i++)
+            {
+                string word = _words[i];
+                if (_wordTokens[i].Capitalize)
+                {
+                    sb.Append(word.ToUpper());
+                }
+                else
+                {
+                    sb.Append(word);
+                }
+                sb.Append(' ');
+            }
+            return sb.ToString().Trim();
         }
 
         public class WordToken

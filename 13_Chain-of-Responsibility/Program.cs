58afd1f185c012d6a574f8c7e78345890aea006b
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-12 17:17:49 +0100

    added Chain of Responsibility template

---
+++ 13_Chain-of-Responsibility/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public abstract class Creature
+    {
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+    }
+
+    public class Goblin : Creature
+    {
+        public Goblin(Game game) { }
+    }
+
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) { }
+    }
+
+    public class Game
+    {
+        public IList<Creature> Creatures;
+    }
+}

665789a8da5884f7069a7e5ad549f67b9357cb9e
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-12 16:01:29 +0100

    added proxy template

---
+++ 12_Proxy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson
+    {
+        public ResponsiblePerson(Person person)
+        {
+            // todo
+        }
+
+        public int Age { /* todo implement property */ }
+    }
+}

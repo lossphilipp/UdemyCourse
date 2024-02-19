5a66291d093cf92b00fbdb20ae2f58af4394f1d5
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-19 16:45:38 +0100

    added interpreter template

---
+++ 15_Interpreter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class ExpressionProcessor
+    {
+        public Dictionary<char, int> Variables = new Dictionary<char, int>();
+
+        public int Calculate(string expression)
+        {
+            // todo
+        }
+    }
+}

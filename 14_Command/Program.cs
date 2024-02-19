c041818bdc3aa7ad454b40af776a094b3655d94c
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-19 16:12:29 +0100

    added command template

---
+++ 14_Command/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Command
+    {
+        public enum Action
+        {
+            Deposit,
+            Withdraw
+        }
+
+        public Action TheAction;
+        public int Amount;
+        public bool Success;
+    }
+
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public void Process(Command c)
+        {
+            // todo
+        }
+    }
+}

9cca032134c96f721d7875c9d382b67973d18ef2
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-20 11:10:47 +0100

    added iterator template

---
+++ 16_Iterator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+
+        public IEnumerable<T> PreOrder
+        {
+            get
+            {
+                // todo!
+            }
+        }
+    }
+}

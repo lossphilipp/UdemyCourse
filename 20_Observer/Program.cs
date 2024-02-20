5f8de829dd740b5e4d79a4eb67af9c7890fd2ab5
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-20 16:59:49 +0100

    implemented Observer

--- 20_Observer/Program.cs
+++ 20_Observer/Program.cs
@@ -1,25 +1,86 @@
-﻿using System;
+﻿using Coding.Exercise;
+using System;
+using System.Collections.Generic;
+
+//Game game = new Game();
+//Rat rat1 = new Rat(game);
+//Rat rat2 = new Rat(game);
+//Rat rat3 = new Rat(game);
+//rat3.Dispose();
+//Console.WriteLine();
 
 namespace Coding.Exercise
 {
     public class Game
     {
-        // todo
-        // remember - no fields or properties!
+        public event EventHandler<EventArgs> RatCountChanged;
+        public event EventHandler<EventArgs> RatScreamingEvent;
+        public event EventHandler<EventArgs> RatDyingEvent;
+
+        public void SpawnRat(Rat rat)
+        {
+            rat.ScreamEvent += RatScreaming;
+            RatCountChanged.Invoke(this, EventArgs.Empty);
+        }
+
+        public void KillRat(Rat rat)
+        {
+            rat.ScreamEvent -= RatScreaming;
+            RatDyingEvent.Invoke(rat, EventArgs.Empty);
+        }
+
+        public void RatScreaming(object sender, EventArgs e)
+        {
+            RatScreamingEvent.Invoke(sender, EventArgs.Empty);
+        }
     }
 
     public class Rat : IDisposable
     {
-        public int Attack = 1;
+        private readonly Game _game;
+        List<Rat> rats = new List<Rat>();
+        
+        public event EventHandler<EventArgs> ScreamEvent;
+        public int Attack => rats.Count;
 
         public Rat(Game game)
         {
-            // todo
+            _game = game;
+            _game.RatCountChanged += Scream;
+            _game.RatScreamingEvent += CountRats;
+            _game.RatDyingEvent += OtherRatDied;
+            _game.SpawnRat(this);
         }
 
         public void Dispose()
         {
-            // todo
+            _game.RatCountChanged -= Scream;
+            _game.RatScreamingEvent -= CountRats;
+            _game.RatDyingEvent -= OtherRatDied;
+            _game.KillRat(this);
+        }
+
+        private void Scream(object sender, EventArgs e)
+        {
+            ScreamEvent.Invoke(this, EventArgs.Empty);
+        }
+
+        private void CountRats(object sender, EventArgs e)
+        {
+            Rat senderRat = (Rat)sender;
+            if (!rats.Contains(senderRat))
+            {
+                rats.Add(senderRat);
+            }
+        }
+
+        private void OtherRatDied(object sender, EventArgs e)
+        {
+            Rat senderRat = (Rat)sender;
+            if (rats.Contains(senderRat))
+            {
+                rats.Remove(senderRat);
+            }
         }
     }
 }

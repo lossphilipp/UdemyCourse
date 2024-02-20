cf9f0da1acd8313df11aaa65ad09b129cf23bd14
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-20 11:49:28 +0100

    added mediator template

---
+++ 17_Mediator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Participant
+    {
+        public int Value { get; set; }
+
+        public Participant(Mediator mediator)
+        {
+            // todo
+        }
+
+        public void Say(int n)
+        {
+            // todo
+        }
+    }
+
+    public class Mediator
+    {
+        // todo
+    }
+}

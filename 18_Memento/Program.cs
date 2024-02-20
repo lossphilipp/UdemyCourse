6db63c080fe3ee6f27bdad4f15595b241098ab9e
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-20 12:49:01 +0100

    added Memento template

---
+++ 18_Memento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        // todo
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            // todo
+        }
+
+        public Memento AddToken(Token token)
+        {
+            // todo (yes, please do both overloads)
+        }
+
+        public void Revert(Memento m)
+        {
+            // todo
+        }
+    }
+}

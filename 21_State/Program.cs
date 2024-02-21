25ea4123bf41053197146fee2e939be9b163a04d
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-21 17:40:36 +0100

    added state template

---
+++ 21_State/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class CombinationLock
+    {
+        public CombinationLock(int[] combination)
+        {
+            // todo
+        }
+
+        // you need to be changing this on user input
+        public string Status;
+
+        public void EnterDigit(int digit)
+        {
+            // todo
+        }
+    }
+}

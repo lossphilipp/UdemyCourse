239dba9e7f6d6de6aeab4b9195bf015a37b7db00
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-21 17:47:36 +0100

    implemented state

--- 21_State/Program.cs
+++ 21_State/Program.cs
@@ -6,15 +6,38 @@ namespace Coding.Exercise
     {
         public CombinationLock(int[] combination)
         {
-            // todo
+            this.combination = combination;
+            Status = "LOCKED";
         }
 
         // you need to be changing this on user input
         public string Status;
+        private readonly int[] combination;
 
         public void EnterDigit(int digit)
         {
-            // todo
+            if (Status == "LOCKED")
+            {
+                if (digit == combination[0])
+                {
+                    Status = digit.ToString();
+                }
+            }
+            else
+            {
+                if (digit == combination[Status.Length])
+                {
+                    Status += digit.ToString();
+                }
+                else
+                {
+                    Status = "ERROR";
+                }
+                if (Status != "LOCKED" && Status != "ERROR" && Status.Length == combination.Length)
+                {
+                    Status = "OPEN";
+                }
+            }
         }
     }
 }

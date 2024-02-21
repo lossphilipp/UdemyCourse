ca0247950f75fee40708a0910205a3d79422e766
lossphilipp <75640684+lossphilipp@users.noreply.github.com>
2024-02-21 18:05:39 +0100

    added strategy template

---
+++ 22_Strategy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace Coding.Exercise
+{
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        // todo
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        // todo (return NaN on negative discriminant!)
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            // todo
+        }
+    }
+}
